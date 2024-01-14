7b0acd60dafa97ad63c95e0d96a2833e5b346b30
pradojvictor <joaovictorred@hotmail.com>
2024-01-14 08:58:55 -0300

    feature: create component card to posts

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+export const PostContainer = styled(Link)`
+   width: 100%;
+   height: 15rem;
+   background-color: #F9F4EC;
+   border-bottom: 2px solid gray;
+   transition: 0.4s;
+   &:hover{
+    background-color: #E2DBCF;
+   }
+   .title-div{
+    display: flex;
+    margin-bottom: 1.25rem;
+    strong{
+        font-size: 2em;
+        font-weight: 600;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+    }
+   }
+   .description-div{
+       p{
+           font-size: 0.9em;
+           overflow: hidden;
+           text-overflow: ellipsis;
+           display: -webkit-box;
+           -webkit-box-orient: vertical;
+           -webkit-line-clamp: 4;
+           margin-bottom: 2rem;
+       }
+    }
+    span{
+        width: max-content;
+        font-size: 0.8rem;
+        color: gray;
+    }
+    .containe-div{
+        height:85%;
+    }
+`;
+
+interface PostsProps {
+    title: string,
+    description: string;
+    date: string;
+    redirect: string; //to
+}
+
+export default function Post(props: PostsProps) {
+    return (
+        <PostContainer to="/post/1">
+            <div className="containe-div">
+                <div className="title-div">
+                    <strong>{props.title}</strong>
+                </div>
+                <div className="description-div">
+                    <p>{props.description}</p>
+                </div>
+            </div>
+            <span>{props.date}</span>
+        </PostContainer>
+    )
+}
