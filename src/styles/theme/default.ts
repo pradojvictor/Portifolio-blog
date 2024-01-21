2a798b2c62805960f983ed51c551fdddffa76a89
pradojvictor <joaovictorred@hotmail.com>
2024-01-21 17:04:42 -0300

    feature: add themes light/dark

--- src/styles/theme/default.ts
+++
@@ -1,6 +0,0 @@
-export const themeDefault = {
-    colors: {
-        "text-primary": "#fafafa",
-        "text-secondary": "#424242"
-    },
-} as const;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,29 @@
+export const themeDefault = {
+    colors: {
+        "text-primary": "#fafafa",
+        "text-secondary": "#424242"
+    },
+    dark: {
+        backgroundColor: '#212121',
+        textColorH1: '#ffffff',
+        textColorH2: '#e0e0e0',
+        textColorH4: '#ffffff',
+        textColorP: '#9e9e9e',
+        textColorLi: '#bdbdbd',
+        textColorStronge: '#FFA500',
+        textColorLink: '#FFA500',
+        textColorLinkHover: '#fff3e0',
+
+        
+    },
+    light: {
+        backgroundColor: ' ',
+        textColorH1: ' ',
+        textColorH2: ' ',
+        textColorH4: ' ',
+        textColorP: ' ',
+        textColorStronge: ' ',
+        textColorLink: '',
+        textColorLinkHover: ' ',
+    }
+} as const;
