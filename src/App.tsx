87a1cece6b600cbae392832d62f15efd06f6ab34
pradojvictor <joaovictorred@hotmail.com>
2024-01-21 15:32:35 -0300

    feature: changer import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 import { ThemeProvider } from "styled-components";
-import { themeDefault } from "./styles/theme/default";
+import { themeDefault } from "./styles/themes/default";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
