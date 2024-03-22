5effeac7b921bd6195438a4133b5b71746f593b6
pradojvictor <joaovictorred@hotmail.com>
2024-03-22 13:20:21 -0300

    feature: add icons

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -3,6 +3,14 @@ import image2 from "./imgs/img2.jpg"
 import image3 from "./imgs/img3.jpg"
 import icon1 from "./icons/icons-moon.png";
 import icon2 from "./icons/icons-sun.png"
+import iconEmail from "./icons/envelope-solid.svg"
+import iconGithub from "./icons/github.svg"
+import iconLinkedin from "./icons/linkedin-in.svg"
+import iconTelegran from "./icons/telegram.svg"
+import iconTwitter from "./icons/twitter.svg"
+import iconLinkedinalt from "./icons/linkedin.svg"
+import icontest from "./icons/teste.svg"
+
 
 export const images = {
     image1,
@@ -12,4 +20,11 @@ export const images = {
 export const icons = {
     icon1,
     icon2,
+    iconEmail,
+    iconGithub,
+    iconLinkedin,
+    iconLinkedinalt,
+    iconTelegran,
+    iconTwitter,
+    icontest,
 }
