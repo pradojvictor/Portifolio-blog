6ba2d08edf44404cb3b45e8a38a8b68e4e025e6a
pradojvictor <joaovictorred@hotmail.com>
2024-03-22 13:25:05 -0300

    feature: css

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,24 @@
 import styled from "styled-components"
+import Slider from "../../components/Slider";
+
 
 export const HomeContaine = styled.div`
-  
+    height: 80vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #38232340;
+    overflow: hidden;
+    box-sizing: border-box;
 `;
+
 export function Home() {
 
 
     return (
         <HomeContaine >
-
+            {/* <ButtonHover /> */}
+            <Slider /> 
         </HomeContaine>
     )
 }
