f504fab4e3c1293065e6f24a2adf352426d007e1
pradojvictor <joaovictorred@hotmail.com>
2025-02-25 00:05:24 -0300

    slider finished, add links

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
-import { images } from "../../assets";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { Key, useCallback, useEffect, useRef, useState } from "react";
 import { api } from "../../lib/axios";
+import Link from "../Link";
 
 export const SliderContainer = styled.div`
     display: flex;
@@ -44,7 +44,7 @@ export const SliderContainer = styled.div`
         }
     }
     .div-card {
-        height: 12rem;
+        height: auto;
         width: 17rem;
         border-radius: 1rem;
         background: rgba( 255, 255, 255, 0.05 );
@@ -56,7 +56,7 @@ export const SliderContainer = styled.div`
         margin-right: 1rem;
         
         .div-info-containe {
-            padding: 1.5rem 1rem;
+            padding: 1rem 1rem;
         }
         .div-inf {
             font-weight: 400;
@@ -77,7 +77,7 @@ export const SliderContainer = styled.div`
             .inf-description {
                 height: 7.5rem;
                 p {
-                    padding-top: 4.5rem;
+                    padding-top: 4rem;
                     font-size: .7rem;
                     text-transform: capitalize;
                     font-family: 'Poppins', sans-serif;
@@ -96,9 +96,9 @@ export const SliderContainer = styled.div`
             justify-content: center;
             align-items: center;
             width: 100%;
-            padding-left: 10rem;
+            padding-left: 8rem;
             button {
-                width: 3rem;  
+                width: 100%;  
                 display: flex;
                 align-items: center;
                 justify-content: center; 
@@ -106,7 +106,7 @@ export const SliderContainer = styled.div`
                 border-style: none;  
                 svg {
                     color: #ffffff;
-                    width: 1.5rem;
+                    width: 2.2rem;
                 }
             }
         }
@@ -117,17 +117,18 @@ export const SliderContainer = styled.div`
 `;
 
 export interface PostProps {
-    title: string,
+    id: Key | null | undefined;
+    name: string,
+    description: string,
     body: string,
     created_at: string,
     number: number,
     html_url: string,
+    homepage: string,
     user: {
         login: string,
     },
 }
-
-//https://api.github.com/users/pradojvictor/starred
 
 const userName = import.meta.env.VITE_GITHUB_USERNAME;
 
@@ -137,11 +138,11 @@ export default function Slider() {
 
     const getRepos = useCallback(async (query: string = "starred") => {
         try {
-            // setIsLoading(true);
+            setRepos([]);
             const response = await api.get(`/users/${userName}/${query}`);
             setRepos(response.data);
         } finally {
-            // setIsLoading(false);
+            setRepos; //obs!!!
         }
     }, []);
 
@@ -174,7 +175,7 @@ export default function Slider() {
                 </button>
             </div>
             <div className="div-slide" ref={slider}>
-                {data.map((d) => (
+                {repos.map((d) => (
                     <div className="div-card" key={d.id}>
                         <div className="div-info-containe">
                             <div className="div-inf">
@@ -182,23 +183,28 @@ export default function Slider() {
                                     <h1>{d.name}</h1>
                                 </div>
                                 <div className="inf-description">
-                                    <p>{d.review}</p>
+                                    <p>{d.description}</p>
                                 </div>
                             </div>
                             <div className="inf-button">
                                 <button>
-                                    <svg viewBox="0 0 1024 1024" fill="currentColor">
-                                        <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
-                                    </svg>
+                                    <Link redirect={d.html_url} target="_blank">
+                                        <svg viewBox="0 0 1024 1024" fill="currentColor">
+                                            <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
+                                        </svg>
+                                    </Link>
                                 </button>
                                 <button>
-                                <svg
-      viewBox="0 0 500 500"
-      fill="currentColor"
-    >
-      <path d="M0 128c0-35.3 28.7-64 64-64h384c35.3 0 64 28.7 64 64v256c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zm64 32v64c0 17.7 14.3 32 32 32h320c17.7 0 32-14.3 32-32v-64c0-17.7-14.3-32-32-32H96c-17.7 0-32 14.3-32 32zm16 160c-13.3 0-24 10.7-24 24s10.7 24 24 24h56c13.3 0 24-10.7 24-24s-10.7-24-24-24H80zm136 0c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24h-48z" />
-    </svg>
+                                    <Link redirect={d.homepage} target="_blank">
+                                        <svg
+                                            viewBox="0 0 500 500"
+                                            fill="currentColor"
+                                        >
+                                            <path d="M0 128c0-35.3 28.7-64 64-64h384c35.3 0 64 28.7 64 64v256c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zm64 32v64c0 17.7 14.3 32 32 32h320c17.7 0 32-14.3 32-32v-64c0-17.7-14.3-32-32-32H96c-17.7 0-32 14.3-32 32zm16 160c-13.3 0-24 10.7-24 24s10.7 24 24 24h56c13.3 0 24-10.7 24-24s-10.7-24-24-24H80zm136 0c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24h-48z" />
+                                        </svg>
+                                    </Link>
                                 </button>
+
                             </div>
                         </div>
                     </div>
@@ -207,49 +213,3 @@ export default function Slider() {
         </SliderContainer>
     )
 }
-
-
-const data = [
-    {
-        id: 1,
-        name: `prado`,
-        img: `${images.image1}`,
-        review: `deu certo esse teste`
-    },
-    {
-        id: 2,
-        name: `lima tested um nome grande demais`,
-        img: `${images.image2}`,
-        review: `deu certo esse teste tmb`
-    },
-    {
-        id: 3,
-        name: `pinheiro`,
-        img: `${images.image3}`,
-        review: `deu certo esse teste de novo To use a keyboard To use a keyboard shortcut to change between lowercase, UPPERCASE, and Capitalize Each Word, select the text and press SHIFT + F3 until the case you want is applied.shortcut to change between lowercase, UPPERCASE, and Capitalize Each Word, select the text and press SHIFT + F3 until the case you want is applied.`
-    },
-    {
-        id: 4,
-        name: `lima lima lima lima lima lima lima lima lima lima`,
-        img: `${images.image2}`,
-        review: `deu certo esse teste tmb eu certo esse teste tmbeu certo esse teste tmbeu certo esse teste tmbeu certo esse teste tmbeu certo esse teste tmbeu certo esse teste tmbeu certo esse teste tmbeu certo esse teste tmbeu certo esse teste tmb`
-    },
-    {
-        id: 5,
-        name: `pinheiro`,
-        img: `${images.image3}`,
-        review: `deu certo esse teste de novo`
-    },
-    {
-        id: 6,
-        name: `lima`,
-        img: `${images.image2}`,
-        review: `deu certo esse teste tmb`
-    },
-    {
-        id: 7,
-        name: `pinheiro`,
-        img: `${images.image3}`,
-        review: `deu certo esse teste de novo`
-    }
-]
